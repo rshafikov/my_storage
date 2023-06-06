14fefcf8625915b99b827cd4707ab19be3d67c5c
Ramil Shafikov <rshafikov.work@yandex.ru>
2023-06-06 22:58:36 +0200

    MVP has finished

--- frontend/my_storage_gui/files/views.py
+++ frontend/my_storage_gui/files/views.py
@@ -1,18 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import FileResponse
 import requests
 from my_storage_gui.settings import API_URL
 import json
 
 
 def index(request):
-    path = request.POST.get('path')
-    if request.POST.get('action') == 'upload' and path:
-        request.session['text'] = request.POST.get('text')
-        return redirect('files:upload', path)
-    elif request.POST.get('action') == 'download' and path:
-        return redirect('files:download', path)
-
     return render(request, template_name='index.html')
 
 
@@ -28,29 +20,38 @@ def download(request, path):
         dir_tree = requests.get(f'{API_URL}/dir').json()
     except requests.RequestException as error:
         api_response = {
-            'exception_type': type(error).__name__,
+            'data': type(error).__name__,
             'error_message': str(error)
         }
         dir_tree = {'error': 'Unavailable'}
 
+    content = api_response.get('data')
     return render(
         request,
         template_name='api_response.html',
         context={
             'response': json.dumps(api_response, indent=4),
             'dir_tree': json.dumps(dir_tree, indent=4),
-            'content': api_response.get('data'),
+            'content': content if len(content) <= 30 else (
+                'Large file. Please download it instead.'),
             'path': path}
     )
 
 
 def upload(request, path):
-    text = request.session.pop('text', 'Please follow to home page.')
+    file = request.FILES.get('file')
+    text = request.POST.get('text', 'empty file content')
+    api_response = {'error': text}
     try:
-        api_response = {'error': text}
-        if text != 'Please follow to home page.':
+        if file:
             api_response = requests.post(
-                f'{API_URL}/upload', data={'path': path, 'text': text}).json()
+                f'{API_URL}/upload',
+                files={'file': file},
+                data={'path': path}).json()
+        else:
+            api_response = requests.post(
+                f'{API_URL}/upload',
+                data={'path': path, 'text': text}).json()
         dir_tree = requests.get(f'{API_URL}/dir').json()
     except Exception('There is error on the back side') as error:
         dir_tree = {'error': 'Unavailable'}
@@ -64,6 +65,6 @@ def upload(request, path):
         context={
             'response': json.dumps(api_response, indent=4),
             'dir_tree': json.dumps(dir_tree, indent=4),
-            'content': text
+            'content': file if file else text
         }
     )
