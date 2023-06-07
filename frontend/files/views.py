8ce69af016ccbe6cc9431de1ec1ce876333c915d
Ramil Shafikov <rshafikov.work@yandex.ru>
2023-06-07 09:54:37 +0200

    deploy files added

---
+++ frontend/files/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, redirect
+import requests
+from my_storage_gui.settings import STORAGE_API_URL
+import json
+
+
+def index(request):
+    return render(request, template_name='index.html')
+
+
+def download(request, path):
+    try:
+        raw = (
+            ('?' + request.GET.get('download') + '=yes') if (
+                request.GET.get('download')) else '')
+        if raw:
+            return redirect(f'{STORAGE_API_URL}/upload{raw}&path={path}')
+        dir_tree = requests.get(f'{STORAGE_API_URL}/dir').json()
+    except requests.RequestException as error:
+        api_response = {
+            'data': type(error).__name__,
+            'error_message': str(error)
+        }
+        dir_tree = {'error': 'Unavailable'}
+
+    content = api_response.get('data')
+    return render(
+        request,
+        template_name='api_response.html',
+        context={
+            'response': json.dumps(api_response, indent=4),
+            'dir_tree': json.dumps(dir_tree, indent=4),
+            'content': content if len(content) <= 30 else (
+                'Large file. Please download it instead.'),
+            'path': path}
+    )
+
+
+def upload(request, path):
+    file = request.FILES.get('file')
+    text = request.POST.get('text', 'empty file content')
+    api_response = {'error': text}
+    try:
+        if file:
+            api_response = requests.post(
+                f'{STORAGE_API_URL}/upload',
+                files={'file': file},
+                data={'path': path}).json()
+        else:
+            api_response = requests.post(
+                f'{STORAGE_API_URL}/upload',
+                data={'path': path, 'text': text}).json()
+        dir_tree = requests.get(f'{STORAGE_API_URL}/dir').json()
+    except Exception('There is error on the back side') as error:
+        dir_tree = {'error': 'Unavailable'}
+        api_response = {
+            'exception_type': type(error).__name__,
+            'error_message': str(error)
+        }
+    return render(
+        request,
+        template_name='api_response.html',
+        context={
+            'response': json.dumps(api_response, indent=4),
+            'dir_tree': json.dumps(dir_tree, indent=4),
+            'content': file if file else text
+        }
+    )
---
+++ frontend/my_storage_gui/settings.py
@@ -0,0 +1,121 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LOG_DIR = '/logs/'
+
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(LOG_DIR, 'django.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'pw7ktv289avetk&1b@$%ia$5_psn0p9#2^0pvo+wx@(eu$0mtz'
+
+# DEBUG = os.getenv('FRONTEND_DEBUG', default=False)
+DEBUG = True
+
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', default='*')]
+# ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'files',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'my_storage_gui.urls'
+
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [TEMPLATES_DIR],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'my_storage_gui.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+STORAGE_API_URL = (
+    f"http://"
+    f"{os.getenv('API_URL', default='localhost')}:"
+    f"{os.getenv('API_PORT', default='5000')}"
+)
